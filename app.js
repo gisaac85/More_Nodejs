9127b385064a954c07acece223dce8dbf08a70d4
Gaorieh Isaac <gaorieh@hotmail.com>
2018-09-08 23:46:02 +0200

    callback

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-let t = require("./triangle");
+//blocking
+let fs = require('fs');
 
-let s = require("./square");
+let data = fs.readFileSync('info.txt');
+
+console.log(data.toString());
+
+//callback
+fs.readFile('info.txt', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+
+});
+
+
+//Async-await
+async function readerFile() {
+    await fs.readFile('info.txt', function (err, data) {
+        if (err) return console.error(err);
+        console.log(data.toString());
+
+    });
+};
+
+readerFile();
+
 
